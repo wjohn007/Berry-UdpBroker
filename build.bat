159fc3bf0a657d79550d6ded772473dce6b3fe9e
j.wagner <wagner.johann@gmx.de>
2024-07-28 01:14:13 +0200

    solid

--- build.bat
+++ build.bat
@@ -19,7 +19,7 @@ set host=192.168.178.126
 :: copy from master repo
 if exist "%copyRoot%" (
 copy /y %copyRoot%\Common\A01DynClass.be .
-copy /y %copyRoot%\Common\xtool_mod.be .
+copy /y %copyRoot%\Common\A01XTool.be .
 copy /y %copyRoot%\Common\G01UdpBroker.be .
 )
 
